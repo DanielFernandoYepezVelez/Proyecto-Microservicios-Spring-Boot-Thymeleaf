4c19f4ee855bbaa9d69be606d3b625beea6399c5
Daniel Fernando Yepez Velez <dfyepes@personalsoft.com.co>
2023-03-27 11:10:38 -0500

    Agregando Archivos Relacionados A Zuul

---
+++ 03-spring-boot-service-zuul-server/src/main/java/com/microservices/springboot/app/zuul/Application.java
@@ -0,0 +1,13 @@
+package com.microservices.springboot.app.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
